f927b05a695531628fb63439cf9797f52b654af5
asimonia <alex.simonian@gmail.com>
2016-06-14 01:42:51 -0500

    Initial commit

---
+++ bloggy_project/blog/models.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+class Post(models.Model):
+	created_at = models.DateTimeField(auto_now_add=True)
+	title = models.CharField(max_length=100)
+	content = models.TextField()
+
+	def __unicode__(self):
+	    return str(self.id) + " / " + str(self.created_at) + " / " + self.title + " / " + self.content + "\n"	
+
---
+++ bloggy_project/blog/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.template import Context, loader
+
+from blog.models import Post
+from django.shortcuts import render
+
+def index(request):
+	latest_posts = Post.objects.all().order_by('-created_at')
+	t = loader.get_template('blog/index.html')
+	c = Context({'latest_posts': latest_posts, })
+	return HttpResponse(t.render(c))
+
+
+
